4514f33393636078292bebd8baf09c5e07eceeb4
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-11-08 16:10:34 +0530

    Create CodeRun.java

---
+++ CodeRun.java
@@ -0,0 +1,95 @@
+package xlreader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.simple.JSONObject;
+
+
+public class CodeRun {
+
+    public static void main(String[] args) {
+
+        readExcelFile();
+
+    }
+
+
+    private static void readExcelFile() {
+        List lstStudents;
+
+        try {
+            FileInputStream excelFile = new FileInputStream(new File("C:\\Users\\DELL\\Desktop\\ATMECS\\Book1.xlsx"));
+            XSSFWorkbook xssfworkbook = new XSSFWorkbook(excelFile);
+
+            Sheet sheet = xssfworkbook.getSheetAt(0);
+            Iterator rows = sheet.iterator();
+
+            lstStudents = new ArrayList();
+
+            int rowNumber = 0;
+            while (rows.hasNext()) {
+                Row currentRow = (Row) rows.next();
+
+                // skip header
+                if (rowNumber == 0) {
+                    rowNumber++;
+                    continue;
+                }
+
+                Iterator cellsInRow = currentRow.iterator();
+
+                ReadExcel stu = new ReadExcel();
+
+                int cellIndex = 0;
+                while (cellsInRow.hasNext()) {
+                    Cell currentCell = (Cell) cellsInRow.next();
+
+                    if (cellIndex == 0) {
+                        stu.setName(currentCell.getStringCellValue());
+                    } else if (cellIndex == 1) { // Name
+                        stu.setAge((int) currentCell.getNumericCellValue());
+                    } else if (cellIndex == 2) {
+                        stu.setMarks((int) currentCell.getNumericCellValue());
+
+                    }
+
+                    cellIndex++;
+                }
+
+                lstStudents.add(stu);
+            }
+
+            // Close WorkBook
+            xssfworkbook.close();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException("FAIL! -> message = " + e.getMessage());
+        }
+
+
+        JSONObject object = new JSONObject();
+        object.put("Personal details", lstStudents);
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter("Excel_Data.json"));
+            bufferedWriter.write(object.toJSONString());
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
+
+
+
